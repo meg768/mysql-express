c6be95fef769989570f3cebb13b5a748ccff4cfb
meg768 <magnus@egelberg.se>
2025-02-12 23:39:28 +0100

    -

--- mysql-express copy.js
+++ mysql-express copy.js
@@ -28,7 +28,6 @@ class Server {
 		});
 
 		this.argv = args.argv;
-		this.pools = {};
 	}
 
 	log() {
@@ -55,29 +54,25 @@ class Server {
 		});
 
 		app.get('/query', async (request, response) => {
-			let { database, ...options } = Object.assign({}, request.body, request.query);
-			let connection = undefined;
-
 			try {
-				connection = await this.getConnection(database);
-
+				let connection = await this.getConnection();
+				let options = Object.assign({}, request.body, request.query);
 				let result = await this.query(connection, options);
-
+				connection.release();
 				response.status(200).json(result);
 			} catch (error) {
 				response.status(404).json(error);
-			} finally {
-				this.releaseConnection(connection);
 			}
 		});
 
 		app.post('/upsert', async (request, response) => {
-			let { database, table, row, rows } = Object.assign({}, request.body, request.query);
 			let connection = undefined;
 
 			try {
-				connection = await this.getConnection(database);
+				connection = await this.getConnection();
 
+				let params = Object.assign({}, request.body, request.query);
+				let { table, row, rows } = params;
 				let result = await this.upsert(connection, table, row || rows);
 
 				response.status(200).json(result);
@@ -165,36 +160,29 @@ class Server {
 		return await promise;
 	}
 
-	getPool(database) {
-		if (this.pools[database] == undefined) {
-			let options = {};
-			options.host = this.argv.host;
-			options.user = this.argv.user;
-			options.password = this.argv.password;
-			options.database = database;
-			options.port = this.argv.port;
+	connect() {
+		let options = {};
+		options.host = this.argv.host;
+		options.user = this.argv.user;
+		options.password = this.argv.password;
+		options.database = this.argv.database;
+		options.port = this.argv.port;
 
-			// Allow multiple statements
-			options.multipleStatements = true;
+		// Allow multiple statements
+		options.multipleStatements = true;
 
-			this.pools[database] = MySQL.createPool(options);
-		}
-		return this.pools[database];
+		this.pool = MySQL.createPool(options);
 	}
 
-	async getConnection(database) {
-		let pool = this.getPool(database);
-
-		let promise = new Promise((resolve, reject) => {
-			pool.getConnection((error, connection) => {
+	async getConnection() {
+		return new Promise((resolve, reject) => {
+			this.pool.getConnection((error, connection) => {
 				if (error) reject(error);
 				else {
 					resolve(connection);
 				}
 			});
 		});
-
-		return await promise;
 	}
 
 	releaseConnection(connection) {
@@ -205,6 +193,8 @@ class Server {
 
 	async run() {
 		try {
+			this.connect();
+
 			this.listen();
 		} catch (error) {
 			console.error(error.stack);
